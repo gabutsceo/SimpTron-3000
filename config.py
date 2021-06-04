5fc50dd7b9d9bb44a00af7ab93ebed27d07a1dfc
NathanRLiu <nathanliu17@gmail.com>
2021-06-04 00:24:40 -0700

    Increased complexity of responses
    * Now uses text2emotion lib to come up with better responses
    (originally committed to original repo on June 3rd, recommitting because I accidentally leaked creds on that one)

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 daEmail = "putyouremailhere@gmail.com" #put discord email here
-daPassword = "putyourpasswordhere1234" #put discord password here
-daUser = "000000000000000000" #put discord user id here
+daPassword = "putyourpasswordhere" #put discord password here
+daUser = "000000000000000000" #Target UserID
 #put list of compliments here
 daComplimentList = ["you're so talented!","you have nice eyes","i am a better person around you", "you make me smile", "you are beautiful", 
 "your laff is aamzing", "you are an attractive female", "I would like to procreate with you, as you have hips that are perfect for bearing children", "you're fun to talk to",
@@ -12,18 +12,29 @@ daComplimentList = ["you're so talented!","you have nice eyes","i am a better pe
 "I wish I could make you laugh like that more often. You're beautiful all the time, but when you smile like that I swear my world stops.", "I cannot believe how incredibly smart you are.",
 "You're that \"Nothing\" when people ask me what I'm thinking about.", "I like your style.", "You have the best laugh.", "I appreciate you.", "You are the most perfect there is.", "You're a great listener.", 
 "How is it that you always look great, even in sweatpants", "Your smile brightens up the most gloomy day.", "These sound like greeting cards", "you always manage to find a way to make me laugh", "your empathy knows no bounds"]
-daChannelList = ["000000000000000000","000000000000000001"]
-daServer = "000000000000000000"
+daDefenseList = ["You are literally so stinky, you have no right to be talking shit please stop.", "That's an incredibly rude statement.", "Are you sure you're not looking in a mirror?", 
+"no one asked", "Your mother is currently engaging in numerous sexual relationships with many individuals most of whom are not your father, and this is leading to the detriment and eventual downfall of your parents' marriage.",
+"Your grandparents hate you.", "I would advise you to stop talking as I am currently engaging in repeated sexual engagements with your mother.", "Dude, you literally suck at everything please be quiet.",
+"Please disengage from the conversation as you are in possession of a negative amount of braincells.", "That's really maen", "Don't talk to my friend like that", "WTF bro", "Stop talking shitter"]
+daAngryResponseList = ["Omg yes pop off queen", "so true", "yes", "fax", ":100:", "yeah go off bestie you tell em"]
+daSurprisedResponseList = ["Wow! That's amazing!", "Incredible!", "That's insane!", "I am surprised at this statement."]
+daHappyResponseList = ["Perfect!", "Fantastic!", "This is why I'm your number one fan"]
+daSadResponseList = ["Oh, that's so sad. ", "That's unfortunate, but you should know that ", "RIP\nbtw "]
+daFearfulResponseList = ["It's gonna be okay, ", "It will be fine, ", "You'll get through this, "]
+#Put list of channel ids
+daChannelList = ["000000000000000000","000000000000000000","000000000000000000"]
+daServer = "000000000000000000"#put the server id
 msgAsReply = True
+msgPingReply = False
 
 
 #You don't need to change anything under this
 daChannelDictionary = dict.fromkeys(daChannelList, "")
 def getCreds():
     return(daEmail,daPassword)
-def getCompliments():
-    return(daComplimentList)
+def getResponses():
+    return(daComplimentList,daDefenseList,daAngryResponseList,daHappyResponseList,daSadResponseList,daFearfulResponseList, daSurprisedResponseList)
 def getScope():
     return(daUser,daChannelDictionary,daServer)
 def getMisc():
-    return(msgAsReply)
+    return(msgAsReply, msgPingReply)
--- discordmsgauto.py
+++ discordmsgauto.py
@@ -1,11 +1,13 @@
 import http.client
 import json,time
 import random
-from config import getCreds, getCompliments, getScope, getMisc
+import text2emotion as te
+from config import getCreds, getResponses, getScope, getMisc
 
 conn = http.client.HTTPSConnection("discord.com")
 username,password = getCreds()
 payload = "{\"login\":\""+username+"\",\"password\":\""+password+"\"}"
+daComplimentList,daDefenseList,daAngryResponseList,daHappyResponseList,daSadResponseList,daFearfulResponseList, daSurprisedResponseList = getResponses()
 
 headers = {
     'cookie': "__cfduid=d34b9649242049e3c95e9737b41c2bf4d1619142338; __dcfduid=327cde81d8964a30887a3d3d92b55dbf",
@@ -19,13 +21,34 @@ data = res.read()
 
 daToken = json.loads(data.decode("utf-8"))["token"]
 daCount = 0
-daComplimentList = getCompliments()
 reactTo,channelIDtoMsg,daGuildID = getScope()
-msgAsReply = getMisc()
+msgAsReply,msgPingReply = getMisc()
 
-def createCompliment():
+def createCompliment(emotions):
+    highest = 0.0
+    daEmotion = ""
     daMessage = ""
-    daMessage += daComplimentList[random.randint(0,len(daComplimentList))-1]
+    for emotion in emotions:
+        if emotions[emotion] > highest:
+            print(emotions[emotion])
+            highest = emotions[emotion]
+            daEmotion = emotion
+    print(daEmotion)
+    daMessage = ""
+    if daEmotion == "Happy":
+        daMessage += daHappyResponseList[random.randint(0,len(daHappyResponseList))-1]
+    elif daEmotion == "Angry":
+        daMessage += daAngryResponseList[random.randint(0,len(daAngryResponseList))-1]
+    elif daEmotion == "Surprise":
+        daMessage += daSurprisedResponseList[random.randint(0,len(daSurprisedResponseList))-1]
+    elif daEmotion == "Sad":
+        daMessage += daSadResponseList[random.randint(0,len(daSadResponseList))-1]
+        daMessage += daComplimentList[random.randint(0,len(daComplimentList))-1]
+    elif daEmotion == "Fear":
+        daMessage += daFearfulResponseList[random.randint(0,len(daFearfulResponseList))-1]
+        daMessage += daComplimentList[random.randint(0,len(daComplimentList))-1]
+    elif daEmotion == "":
+        daMessage += daComplimentList[random.randint(0,len(daComplimentList))-1]
     return daMessage
 
 while True:
@@ -43,16 +66,22 @@ while True:
         
         res = conn.getresponse()
         data = res.read()
-        if json.loads(data.decode("utf-8"))[0] != channelIDtoMsg[channelID]:
+        if json.loads(data.decode("utf-8"))[0] == channelIDtoMsg[channelID]:
             daMessageID = json.loads(data.decode("utf-8"))[0]["id"]
+            daMessageText = json.loads(data.decode("utf-8"))[0]["content"]
             print(json.loads(data.decode("utf-8"))[0])
             channelIDtoMsg[channelID] = json.loads(data.decode("utf-8"))[0]
-            if channelIDtoMsg[channelID]["author"]["id"] == reactTo:
-                print(daMessageID)
+            if channelIDtoMsg[channelID]["author"]["id"] != reactTo:
+                print(daMessageText)
+                print(te.get_emotion(daMessageText))
+                daEmotionParam = te.get_emotion(daMessageText)
                 if msgAsReply == True:
-                    payload = "{\"content\": \""+createCompliment()+"\",\n\"nonce\": "+str(daCount)+",\n\"tts\": false,\n\"message_reference\": {\"channel_id\": \""+str(channelID)+"\",\n\"guild_id\":\""+str(daGuildID)+"\", \n \"message_id\": \""+str(daMessageID)+"\"}\n}"
+                    payload = "{\"content\": \""+createCompliment(daEmotionParam)+"\",\n\"nonce\": "+str(daCount)+",\n\"tts\": false,\n\"message_reference\": {\"channel_id\": \""+str(channelID)+"\",\n\"guild_id\":\""+str(daGuildID)+"\", \n \"message_id\": \""+str(daMessageID)+"\"},\n"
+                    if msgPingReply == False:
+                        payload += "allowed_mentions: {parse: [\"users\", \"roles\", \"everyone\"], \n replied_user: false}"
+                    payload += "}"
                 else:
-                    payload = "{\"content\": \""+createCompliment()+"\",\n\"nonce\": "+str(daCount)+",\n\"tts\": false}"
+                    payload = "{\"content\": \""+createCompliment(daEmotionParam)+"\",\n\"nonce\": "+str(daCount)+",\n\"tts\": false}"
                 print("Payload:"+payload)
                 conn.request("POST", "/api/v9/channels/"+channelID+"/messages", payload, headers)
                 daCount += 1
